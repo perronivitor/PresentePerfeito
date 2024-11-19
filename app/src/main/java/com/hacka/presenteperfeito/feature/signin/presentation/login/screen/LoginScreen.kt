46340c4fc05f5ec79df98f9ab53972e574825ea7
Hernani S. Boaski <hernanistn@gmail.com>
2024-11-18 23:15:55 -0300

    package: add koin annotation package and set example at login structure

--- app/src/main/java/com/hacka/presenteperfeito/feature/signin/presentation/login/screen/LoginScreen.kt
+++ app/src/main/java/com/hacka/presenteperfeito/feature/signin/presentation/login/screen/LoginScreen.kt
@@ -1,118 +1,104 @@
 package com.hacka.presenteperfeito.feature.signin.presentation.login.screen
 
-import androidx.compose.foundation.Image
+import android.widget.Toast
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Email
-import androidx.compose.material.icons.outlined.Lock
-import androidx.compose.material3.Button
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.hacka.presenteperfeito.R
 import com.hacka.presenteperfeito.core.designSystem.PerfectGiftTheme
-import com.hacka.presenteperfeito.core.designSystem.components.textFields.PrimaryTextField
-import com.hacka.presenteperfeito.feature.signin.presentation.login.uiState.LoginUiState
+import com.hacka.presenteperfeito.core.designSystem.components.buttons.ProjectButton
+import com.hacka.presenteperfeito.core.designSystem.components.buttons.ProjectButtonTypes
+import com.hacka.presenteperfeito.core.di.AppModule
+import com.hacka.presenteperfeito.feature.signin.presentation.login.uiState.LoginEvents
 import com.hacka.presenteperfeito.feature.signin.presentation.login.viewModel.LoginViewModel
+import org.koin.compose.KoinApplication
+import org.koin.compose.viewmodel.koinViewModel
+import org.koin.ksp.generated.module
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-
-fun LoginScreen(viewModel: LoginViewModel = androidx.lifecycle.viewmodel.compose.viewModel()) {
-    val uiState: LoginUiState by viewModel.currentUiState.collectAsState()
-    Scaffold(
-        containerColor = MaterialTheme.colorScheme.primary,
-        contentWindowInsets = WindowInsets(left = 16.dp, right = 16.dp)
-    ) { paddingValues ->
+fun LoginScreen(viewModel: LoginViewModel = koinViewModel()) {
+    val uiState = viewModel.uiState.collectAsState()
+    uiState.value.event?.let { HandleEvents(event = it, onFinish = viewModel::clearEvents) }
+    Scaffold(topBar = {
+        TopAppBar(navigationIcon = {
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(imageVector = Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "")
+            }
+        }, title = { Text(text = "Top App Bar") })
+    }) { paddingValues ->
         Column(
             modifier = Modifier
                 .padding(paddingValues)
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally
+                .background(color = MaterialTheme.colorScheme.primary)
+                .fillMaxSize()
         ) {
-            Spacer(modifier = Modifier.height(84.dp))
-
-            Image(
-
-                modifier = Modifier.size(132.dp, 135.dp),
-                painter = painterResource(id = R.drawable.logo_gift_ic),
-                contentDescription = ""
-            )
-            Text(style = MaterialTheme.typography.headlineMedium, text = "Presente Perfeito")
-
-            Spacer(modifier = Modifier.height(87.dp))
-
-
-            PrimaryTextField(leftIcon = {
-                Icon(
-                    tint = MaterialTheme.colorScheme.primary,
-                    imageVector = Icons.Outlined.Email,
-                    contentDescription = ""
-                )
-            },
-                modifier = Modifier.fillMaxWidth(),
-                value = uiState.email ?: "",
+            TextField(label = { Text(text = "Email") },
+                value = uiState.value.email ?: "",
                 onValueChange = { text ->
                     viewModel.setEmail(text)
-
                 })
-
-            Spacer(modifier = Modifier.height(14.dp))
-
-            PrimaryTextField(leftIcon = {
-                Icon(
-                    tint = MaterialTheme.colorScheme.primary,
-                    imageVector = Icons.Outlined.Lock,
-                    contentDescription = ""
-                )
-            },
-                isPassword = true,
-                modifier = Modifier.fillMaxWidth(),
-                value = uiState.password ?: "",
+            TextField(label = { Text(text = "Password") },
+                value = uiState.value.password ?: "",
                 onValueChange = { text ->
                     viewModel.setPassword(text)
                 })
-            Spacer(modifier = Modifier.height(97.dp))
 
-
-            Button(modifier = Modifier.fillMaxWidth(), onClick = { /*TODO*/ }) {
-                Text(text = "Entrar")
-
+            var isLoading by remember {
+                mutableStateOf(false)
             }
 
-            Spacer(modifier = Modifier.height(57.dp))
-
-            Text(text = "Não possui conta? Cadastre-se aqui")
-
+            ProjectButton(modifier = Modifier.fillMaxWidth(),
+                text = "Entrar",
+                isLoading = isLoading,
+                buttonType = ProjectButtonTypes.Secondary,
+                onButtonClick = {
+                    viewModel.doLogin()
+                })
         }
-
     }
-
 }
 
 @Composable
-@Preview(showBackground = true)
-private fun Preview() {
-    PerfectGiftTheme {
-        LoginScreen()
-    }
+fun HandleEvents(event: LoginEvents, onFinish: () -> Unit) {
+    val context = LocalContext.current
+    when (event) {
+        is LoginEvents.LoginError -> Toast.makeText(context, event.message, Toast.LENGTH_SHORT)
+            .show()
 
+        is LoginEvents.LoginSuccessfully -> Unit //Navegar para home
+    }
+    onFinish()
 }
 
+@Preview
+@Composable
+private fun LoginScreenPreview() {
+    PerfectGiftTheme {
+        KoinApplication(application = {
+            modules(AppModule().module)
+        }) {
+            LoginScreen()
+        }
+    }
+}
