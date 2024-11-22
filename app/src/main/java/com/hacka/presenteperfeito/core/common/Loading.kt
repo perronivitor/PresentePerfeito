08283eedf15c56fa882373f46f1eb800fc181dc5
Hernani S. Boaski <hernanistn@gmail.com>
2024-11-21 23:19:14 -0300

    fix: remove unused firstLoadingError state

--- app/src/main/java/com/hacka/presenteperfeito/core/common/Loading.kt
+++ app/src/main/java/com/hacka/presenteperfeito/core/common/Loading.kt
@@ -3,12 +3,10 @@ package com.hacka.presenteperfeito.core.common
 /**
  * [FirstLoading] typically used for active fullscreen loadings
  * [Processing] typically used for active progress bars or buttons circular loading states.
- * [FirstLoadingError] typically user for show to UI the error and pop the UI.
  * [Complete] loading state is not showing in UI
  */
 enum class Loading {
     FirstLoading,
-    FirstLoadingError,
     Processing,
     Complete
 }
