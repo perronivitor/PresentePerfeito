a5739098e54251bf0dc50831f7647797760fc5ec
perronivitor <vitorope48@gmail.com>
2024-11-22 00:07:46 -0300

    feature: add compose compiler

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,10 +1,9 @@
-import java.io.FileInputStream
-import java.util.Properties
-
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.ksp)
+    kotlin("plugin.serialization") version "2.0.20"
+    alias(libs.plugins.compose.compiler)
 }
 
 ksp {
@@ -110,4 +109,5 @@ dependencies {
     testImplementation(libs.koin.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation(libs.kotlinx.serialization.json)
 }
