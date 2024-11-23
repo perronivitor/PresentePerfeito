a890c5782f39440eddecdd0196394c093c7debb7
perronivitor <vitorope48@gmail.com>
2024-11-23 02:06:42 -0300

    feature: add navigation

--- app/src/main/java/com/hacka/presenteperfeito/MainActivity.kt
+++ app/src/main/java/com/hacka/presenteperfeito/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.hacka.presenteperfeito
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
@@ -17,6 +19,7 @@ import com.hacka.presenteperfeito.core.common.navigation.PerfectGiftNavHost
 import com.hacka.presenteperfeito.core.designSystem.PerfectGiftTheme
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
